ed3e57310af55740c3166c17348fe2f46b40e2a6
Fernando <fmeji@hotmail.es>
2023-02-14 16:07:10 +0100

    Ejercicio1 v1

---
+++ src/main/java/Ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    //Escriba un algoritmo que muestre "riesgo de hielo", si la temperatura introducida es inferior a 2°C.
+
+    public static void main(String[] args) {
+        int temperatura;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la temperatura");
+        temperatura = entrada.nextInt();
+        if (temperatura < 2) {
+            System.out.println("Riesgo de hielo");
+        }else{
+            System.out.println("No hay riesgo de hielo");
+        }
+    }
+
+}
